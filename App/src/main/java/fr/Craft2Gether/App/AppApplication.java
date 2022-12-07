158af505e23b6ba93ba9ad0057b921a840f489ab
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2022-12-07 20:26:40 +0100

    First commit : on ajoute les fichiers générés par spring initialzr

---
+++ App/src/main/java/fr/Craft2Gether/App/AppApplication.java
@@ -0,0 +1,13 @@
+package fr.Craft2Gether.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
