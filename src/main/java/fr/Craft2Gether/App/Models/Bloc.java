300a7a7f4bfd9da4b1e92a56bc19b1a47a8ac51a
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2022-12-07 21:25:16 +0100

    Models : Bloc

--- src/main/java/fr/Craft2Gether/App/Models/Bloc.java
+++ src/main/java/fr/Craft2Gether/App/Models/Bloc.java
@@ -17,5 +17,21 @@ public class Bloc {
     @EqualsAndHashCode.Exclude
     private Long id;
 
+    private String name;
+    private String spawnLocation;
+
+    private BLOC_TYPES type;
+    public enum BLOC_TYPES {
+        BLOC_NATUREL,
+        VEGETATION,
+        MINERAI,
+        ELEMENT_MECANIQUE,
+        BLOC_MANUFACTURE,
+        LIQUIDE,
+        BLOC_COLORE,
+        OBJET,
+        NETHER;
+    }
+
 
 }
