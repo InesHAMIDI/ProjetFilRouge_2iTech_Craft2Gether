3d97e4c7faabe0ef51efce64b3b04b9183fc6bb1
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2022-12-31 15:29:35 +0100

    maj

---
+++ src/main/java/org/eclipse/FilRougeInesHamidiApplication.java
@@ -0,0 +1,13 @@
+package org.eclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeInesHamidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeInesHamidiApplication.class, args);
+	}
+
+}
