c2ac64f7fc6f5a14461abf12bcbd0f7f9988b597
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2022-12-31 16:01:32 +0100

    new version propre

---
+++ src/test/java/org/eclipse/ProjetFilRouge2iTechCraft2GetherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFilRouge2iTechCraft2GetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
