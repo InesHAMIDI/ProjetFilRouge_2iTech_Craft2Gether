21473113af4ff101b4343bcb7ca8ea6f902b00a7
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-25 16:14:16 +0100

    user model

--- src/main/java/fr/crafttogether/model/User.java
+++ src/main/java/fr/crafttogether/model/User.java
@@ -1,11 +1,21 @@
 package fr.crafttogether.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@RequiredArgsConstructorpublic class User {
+@RequiredArgsConstructor
+public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
