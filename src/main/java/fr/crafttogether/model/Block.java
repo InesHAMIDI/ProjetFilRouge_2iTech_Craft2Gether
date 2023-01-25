74e3f2571df705c92341f06d613b6f3c0cc393d3
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-25 11:07:51 +0100

    rangement propre (le pomxml buguait) + model block

--- src/main/java/fr/crafttogether/model/Block.java
+++ src/main/java/fr/crafttogether/model/Block.java
@@ -1,5 +1,40 @@
 package fr.crafttogether.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Block {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@NonNull
+	private String spawnLocation;
+	@NonNull
+	private String nom;
+	@NonNull
+	private Type type;
+	
+	@ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.MERGE})
+	@JsonIgnoreProperties(value = "recipes")
+	private List<Recipe> recipes;
+	
+	private Modder creator;
 }
---
+++ src/main/java/fr/crafttogether/model/Type.java
@@ -0,0 +1,6 @@
+package fr.crafttogether.model;
+
+public enum Type {
+	NATURAL,
+	OBJECT
+}
