3a03cfaf1663c60a06f9dedc7a356f4cc2907794
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-01-27 15:21:43 +0100

    RecipeService

--- src/main/java/fr/crafttogether/service/RecipeService.java
+++ src/main/java/fr/crafttogether/service/RecipeService.java
@@ -2,41 +2,40 @@ package fr.crafttogether.service;
 
 import java.util.List;
 
+import fr.crafttogether.dao.BlockRepository;
 import fr.crafttogether.dao.RecipeRepository;
 import fr.crafttogether.model.Recipe;
 
 public class RecipeService implements GenericService<Recipe, Integer> {
 
 	RecipeRepository recipeRepository;
+	BlockRepository blockRepository;
 	
 	@Override
 	public List<Recipe> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return recipeRepository.findAll();
 	}
 
 	@Override
 	public Recipe findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return recipeRepository.findById(id).orElse(null);
 	}
 
 	@Override
 	public Recipe save(Recipe elt) {
-		// TODO Auto-generated method stub
-		return null;
+		blockRepository.saveAll(elt.getComponents());
+		return recipeRepository.save(elt);
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		recipeRepository.deleteById(id);
 		
 	}
 
 	@Override
 	public Recipe update(Recipe elt) {
-		// TODO Auto-generated method stub
-		return null;
+		return recipeRepository.save(elt);
 	}
 
 }
