3097dc01cb64b50fade3194704996d7915ad05d2
Ines HAMIDI <ineshamid@gmail.com>
2023-03-11 18:16:30 +0100

    Recette model : Changement du type de l'id

--- Craft2Gether/src/main/java/fr/crafttogether/models/Recette.java
+++ Craft2Gether/src/main/java/fr/crafttogether/models/Recette.java
@@ -15,7 +15,7 @@ public class Recette {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private int id;
     private String nom;
     @ManyToMany
     private Map<Integer, Bloc> ingredients;
