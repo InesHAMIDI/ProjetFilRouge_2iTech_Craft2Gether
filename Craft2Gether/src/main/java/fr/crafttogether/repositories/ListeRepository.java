71353849569550126328e38e6866e2393d4ae88d
Ines HAMIDI <ineshamid@gmail.com>
2023-03-11 18:16:16 +0100

    Changement du type de l'id

--- Craft2Gether/src/main/java/fr/crafttogether/repositories/ListeRepository.java
+++ Craft2Gether/src/main/java/fr/crafttogether/repositories/ListeRepository.java
@@ -3,5 +3,5 @@ package fr.crafttogether.repositories;
 import fr.crafttogether.models.Liste;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListeRepository extends JpaRepository<Liste, Long> {
+public interface ListeRepository extends JpaRepository<Liste, Integer> {
 }
