066e6b6ff00df008a789b7d2cd8520a38d4222cc
Ines HAMIDI <ineshamid@gmail.com>
2023-03-11 18:14:46 +0100

    tentative de commandLineRunner

--- Craft2Gether/src/main/java/fr/crafttogether/Craft2GetherApplication.java
+++ Craft2Gether/src/main/java/fr/crafttogether/Craft2GetherApplication.java
@@ -1,9 +1,11 @@
 package fr.crafttogether;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class Craft2GetherApplication {
 
     public static void main(String[] args) {
