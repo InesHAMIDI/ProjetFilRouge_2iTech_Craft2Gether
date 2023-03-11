353e8c0b667a15822f0474c86c7d4bc62b33090b
Ines HAMIDI <ineshamid@gmail.com>
2023-03-11 18:16:03 +0100

    Changement du type de l'id

--- Craft2Gether/src/main/java/fr/crafttogether/repositories/BlocRepository.java
+++ Craft2Gether/src/main/java/fr/crafttogether/repositories/BlocRepository.java
@@ -3,5 +3,5 @@ package fr.crafttogether.repositories;
 import fr.crafttogether.models.Bloc;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlocRepository extends JpaRepository<Bloc, Long> {
-        }
+public interface BlocRepository extends JpaRepository<Bloc, Integer> {
+}
