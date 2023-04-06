8221c9e11337b7d4eb7891d98ae4d43eded762f5
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-04-06 10:19:40 +0200

    changement dans RecetteRepository, on change le type de l'id

--- Craft2Gether/src/main/java/fr/crafttogether/repositories/RecetteRepository.java
+++ Craft2Gether/src/main/java/fr/crafttogether/repositories/RecetteRepository.java
@@ -3,5 +3,5 @@ package fr.crafttogether.repositories;
 import fr.crafttogether.models.Recette;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecetteRepository extends JpaRepository<Recette, Long>  {
+public interface RecetteRepository extends JpaRepository<Recette, Integer>  {
 }
