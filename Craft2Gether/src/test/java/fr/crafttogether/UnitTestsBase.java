de78577615109311765ce8ab7ae250af95214776
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-04-06 16:47:39 +0200

    Les tests sont OK mais cassent à cause de la sécurité (normal)

--- Craft2Gether/src/test/java/fr/crafttogether/UnitTestsBase.java
+++ Craft2Gether/src/test/java/fr/crafttogether/UnitTestsBase.java
@@ -1,9 +1,25 @@
 package fr.crafttogether;
+import fr.crafttogether.repositories.BlocRepository;
+import fr.crafttogether.repositories.ListeRepository;
+import fr.crafttogether.repositories.RecetteRepository;
+import fr.crafttogether.repositories.UserRepository;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("unit-tests")
 public class UnitTestsBase {
 
+    @MockBean
+    protected UserRepository userRepository;
+
+    @MockBean
+    protected BlocRepository blocRepository;
+
+    @MockBean
+    protected ListeRepository listeRepository;
+
+    @MockBean
+    protected RecetteRepository recetteRepository;
 }
--- Craft2Gether/src/test/java/fr/crafttogether/controllers/RecetteControllerTest.java
+++ Craft2Gether/src/test/java/fr/crafttogether/controllers/RecetteControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -44,6 +45,7 @@ class RecetteControllerTest extends UnitTestsBase {
     }
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     void testGetRecettesSuccess() throws Exception{
         // Arrange
         List<Recette> dummyRecettes = List.of(
