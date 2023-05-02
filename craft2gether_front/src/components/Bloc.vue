7553060e1372244337d52ca6d6830d435768bdc5
Ines HAMIDI <ineshamid@gmail.com>
2023-05-02 16:32:17 +0200

    debut du front avec les premiers composants : Bloc et Recette

---
+++ craft2gether_front/src/components/Bloc.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ bloc.nom }} {{ bloc.biomeOrigine }} {{ bloc.outilNecessaire }} {{ bloc.type }}
+    </div>
+</template>
+<script>
+export default {
+    name: "BlocComponent",
+    props:{
+        bloc:  { }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
