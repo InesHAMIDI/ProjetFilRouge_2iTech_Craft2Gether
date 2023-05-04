9cd4ce2b331d115d7645bfe171637c188f7a669f
Ines HAMIDI <ineshamid@gmail.com>
2023-05-04 12:31:02 +0200

    on retire naivee ui du code

--- craft2gether_front/src/components/Liste.vue
+++ craft2gether_front/src/components/Liste.vue
@@ -1,20 +1,5 @@
 <template>
-  <n-table :single-line="false">
-    <thead>
-      <tr>
-        <th>Liste</th>
-        <th>Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{liste}}</td>
-      </tr>
-      <tr>
-        <td>{{ liste.status }}</td>
-      </tr>
-    </tbody>
-  </n-table>
+  mettre tablo
 </template>
 
 <script>
