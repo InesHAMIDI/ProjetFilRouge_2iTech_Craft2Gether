504917feeee0e79262f2b42629b9e4cc43c8afc7
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-05-20 15:19:22 +0200

    le user s'affiche mais pas le logo

--- craft2gether_front/src/components/HeaderComponent.vue
+++ craft2gether_front/src/components/HeaderComponent.vue
@@ -2,8 +2,8 @@
     <div class="header">
         <nav class="navbar bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><i class="fal block-brick"></i></a>
-                <a class="d-flex" href="#"><i class="fa user"></i></a>
+                <a class="navbar-brand" href="#"><i class="fa-light fa-block-brick"></i></a>
+                <a class="d-flex" href="user"><i class="fa-regular fa-user"></i></a>
                 <form class="d-flex" role="search">
                     <input class="form-control me" type="search" placeholder="Recette ou Bloc" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Rechercher</button>
