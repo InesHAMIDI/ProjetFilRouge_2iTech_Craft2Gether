db86b2e04b1d26d8193cb933e486528102a44bcc
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-05-26 09:55:53 +0200

    form pour créer une liste

--- craft2gether_front/src/views/ListeCreateView.vue
+++ craft2gether_front/src/views/ListeCreateView.vue
@@ -1,11 +1,48 @@
 <template>
     <div>
+        <h2>Créer une liste</h2>
+        <Form @submit="creerListe" ref="listeForm">
+        <div>
+            <label for="titre">Titre</label>
+            <Field id="titre" type="text" name="titre" :rules="validateTitre"/>
+            <ErrorMessage name="titre" />
+        </div>
         
+        <button>Valider</button>
+    </Form>
     </div>
 </template>
 <script>
+import { Field, Form, ErrorMessage } from 'vee-validate'
+import * as yup from 'yup'
 export default {
-    
+    components: {
+        Field,
+        Form,
+        ErrorMessage
+    },
+
+    data() {
+        return {
+            validateTitre: yup
+                .string()
+                .required("Champ Obligatoire"),
+            
+            erreur: ""
+        }
+    },
+
+    methods: {
+        creerListe(values) {
+            values.adresses= []
+            this.axios.post(`${this.baseUrl}/listes`, values)
+                .then(response => {
+                    this.$emit('sendData', response.data)
+                    this.$refs.listeForm.resetForm()
+                })
+                .catch(err => this.erreur = err);
+        },
+    },
 }
 </script>
 <style scoped>
