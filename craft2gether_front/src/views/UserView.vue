d3689d68966c4dff4055d9804daff22213418719
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-05-28 15:22:48 +0200

    blloop

---
+++ craft2gether_front/src/views/UserView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="user-card">
+        <div class="card">
+            <img class="card-img-top" :src="profilePic" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">{{ user.name }}</h5>
+            <p class="card-text">Listes : {{ nombredeListes }}</p>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    components:{
+        
+    },
+    data(){
+        return{
+            user:{},
+            profilePic: ""
+        }
+    },
+    computed:{
+        nombredeListes(){
+            if(this.user.listes == undefined || this.user.listes == null)
+                return 0;
+            else
+                return this.user.listes.lenght;
+        }
+    },
+    mounted() {
+        this.axios.get(`${this.baseUrl}/users/${this.id}`)
+            .then(response => this.user = response.data)
+            .catch(err => this.erreur = err);
+    },
+}
+
+</script>
+<style scoped>
+    .user-card{
+        align-items: center;
+        border:2px;
+    }
+</style>
